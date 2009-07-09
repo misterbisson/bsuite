c548b7d87b15c6d832204a5adb9ee84461759b73
Casey Bisson <casey.bisson@gmail.com>
2009-07-09 19:54:58 +0000

    added a title only widget template
    git-svn-id: http://plugins.svn.wordpress.org/bsuite/trunk@133429 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ templates/d_default_titleonly.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Template Name: Default Excerpt Tiny
+*/
+?>
+
+<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
+	<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a> <small><?php the_time('F jS, Y') ?></small></h2>
+</div>
