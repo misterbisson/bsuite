9e96c75a58ff9127c811238e8443684b63b70663
Casey Bisson <casey.bisson@gmail.com>
2010-12-24 00:52:27 +0000

    fixing wijax so that it displays the correct widget instance
    git-svn-id: http://plugins.svn.wordpress.org/bsuite/trunk@325797 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- components/wijax.php
+++ components/wijax.php
@@ -41,25 +41,33 @@ class bSuite_Wijax {
 		{
 			if( ! $widget_data = $wp_registered_widgets[ $key ] )
 				continue;
-	
+
+			preg_match( '/\-([0-9]+)$/' , $key , $instance_number );
+			$instance_number = absint( $instance_number[1] );
+			if( ! $instance_number )
+				continue;
+
 			$widget_data['widget'] = $key;
 	
 			$widget_data['params'][0] = array(
-				'name' => 'a',
-				'id' => 'a',
+				'name' => $wp_registered_widgets[ $key ]['name'],
+				'id' => $key,
 				'before_widget' => '<div id="widget-%1$s" class="widget %2$s"><div class="widget-inner">'."\n",
 				'after_widget'  => '</div></div>'."\n",
 				'before_title'  => '<h2 class="widgettitle">',
 				'after_title'   => "</h2>\n",
-				'widget_id' => 'a',
-				'widget_name' => 'a',
+				'widget_id' => $key,
+				'widget_name' => $wp_registered_widgets[ $key ]['name'],
 			);
-	
+
+//print_r( $widget_data['callback'][0]->number );
+
 			$widget_data['params'][1] = array(
-				'number' => absint( $widget_data['callback'][0]->number ),
+				'number' => absint( $instance_number ),
 			);
 	
 			$widget_data['params'][0]['before_widget'] = sprintf($widget_data['params'][0]['before_widget'], $widget_data['widget'], 'grid_' . $widget_data['size'] . ' ' .$widget_data['class'] . ' ' . $widget_data['id'] . ' ' . $extra_classes);
+
 			call_user_func_array( $widget_data['callback'], $widget_data['params'] );
 
 		}//end foreach
