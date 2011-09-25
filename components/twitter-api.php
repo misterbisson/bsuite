2e9eb7f3a35afb9e3f00d0a756ce7f8227a6ce24
Casey Bisson <casey.bisson@gmail.com>
2011-09-25 21:38:45 +0000

    WIP twitter user stream class. It's untested and will probably fatal.
    git-svn-id: http://plugins.svn.wordpress.org/bsuite/trunk@443370 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- components/twitter-api.php
+++ components/twitter-api.php
@@ -2,9 +2,12 @@
 
 /*
  * Twitter_Search class
+ * 
+ * Search Twitter with a given term or phrase
+ * Example: $twitter_search->search ( array( 'q' => 'search phrase' )) 
+ * 
  * @author Casey Bisson
  */
-
 class Twitter_Search
 {
 	function tweets()
@@ -18,7 +21,7 @@ class Twitter_Search
 	function next()
 	{
 		if( ! empty( $this->api_response->next_page ))
-			$this->search( 'next' );
+			$this->search( $this->args , 'next' );
 		else
 			return FALSE;
 	}
@@ -26,34 +29,35 @@ class Twitter_Search
 	function refresh()
 	{
 		if( ! empty( $this->api_response->refresh_url ))
-			$this->search( 'refresh' );
+			$this->search( $this->args , 'refresh' );
 		else
 			return FALSE;
 	}
 
 	function search( $args , $method = 'search' )
 	{
+		// parse the method
 		switch( $method )
 		{
 			case 'next':
 			case 'next_page':
 				if( ! empty( $this->api_response->next_page ))
 				{
-					$query_url = 'http://search.twitter.com/search.json' . $this->search_api_response->next_page;
+					$query_url = 'http://search.twitter.com/search.json' . $this->api_response->next_page;
 					break;
 				}
 			
 			case 'refresh':
 				if( ! empty( $this->api_response->refresh_url ))
 				{
-					$query_url = 'http://search.twitter.com/search.json' . $this->search_api_response->refresh_url;
+					$query_url = 'http://search.twitter.com/search.json' . $this->api_response->refresh_url;
 					break;
 				}
 
 			case 'search':
 			default:
 				$defaults = array(
-					'q' => urlencode( home_url()),
+					'q' => urlencode( home_url() ),
 					'rpp' => 10,
 					'result_type' => 'recent',
 					'page' => 1,
@@ -65,6 +69,10 @@ class Twitter_Search
 					'show_user' => FALSE,
 				);
 				$args = wp_parse_args( $args, $defaults );
+
+				// save the args
+				$this->args = $args;
+
 				$query_url = add_query_arg( $args , 'http://search.twitter.com/search.json' );
 		}
 
@@ -78,9 +86,9 @@ class Twitter_Search
 		$this->api_response = json_decode( wp_remote_retrieve_body( $temp_results ));
 		unset( $temp_results );
 
-		if( ! empty( $user->error ))
+		if( ! empty( $this->api_response->error ))
 		{
-			$this->error = $this->search_api_response; 
+			$this->error = $this->api_response; 
 			unset( $this->api_response );
 			return FALSE;
 		}
@@ -100,15 +108,29 @@ class Twitter_Search
 	}
 }
 
+/*
+ * twitter_user_info
+ * 
+ * Get the public information for a given user
+ * Example: twitter_user_info( 'misterbisson' ) 
+ * 
+ * @author Casey Bisson
+ */
 function twitter_user_info( $screen_name , $by = 'screen_name' )
 {
-	//Look up info about the twitter user by their screen name (the from_user_id is only valid within the search API)
-	//method docs: http://apiwiki.twitter.com/Twitter-REST-API-Method%3A-users%C2%A0show
-	//useful: $user->name, $user->screen_name, $user->id_str, $user->followers_count 
+	// Look up info about the twitter user by their screen name or ID
+	// Note: the ID here is not compatible with the user ID returned from the search API. This is a Twitter limitation.
+	// method docs: http://apiwiki.twitter.com/Twitter-REST-API-Method%3A-users%C2%A0show
+	// useful: $user->name, $user->screen_name, $user->id_str, $user->followers_count 
 
-	if ( ! $user = wp_cache_get( (string) $screen_name , 'twitter_screen_name' ))
+	// are we searching by screen name or ID?
+	$by = in_array( $by , array( 'screen_name' , 'id' )) ? $by : 'screen_name';
+
+	// check the cache for the user info
+	if ( ! $user = wp_cache_get( (string) $screen_name , 'twitter_'. $by ))
 	{
-		$temp_results = wp_remote_get( 'http://api.twitter.com/1/users/show.json?screen_name='. urlencode( $screen_name ) );
+		// check Twitter for the user info
+		$temp_results = wp_remote_get( 'http://api.twitter.com/1/users/show.json?'. $by .'='. urlencode( $screen_name ) );
 		if ( is_wp_error( $temp_results ))
 			return FALSE;
 
@@ -119,6 +141,109 @@ function twitter_user_info( $screen_name , $by = 'screen_name' )
 	}
 
 	return $user;
+}
+
+/*
+ * Twitter_User_Stream class
+ * 
+ * Get the public Twitter history for a given user
+ * Example: $twitter_search->search ( array( 'q' => 'search phrase' )) 
+ * 
+ * @author Casey Bisson
+ */
+class Twitter_User_Stream
+{
+	function tweets()
+	{
+		if( ! empty( $this->api_response->results ))
+			return $this->api_response->results;
+		else
+			return FALSE;
+	}
+
+	function next()
+	{
+		if( ! empty( $this->api_response->results ))
+			$this->stream( $this->args , 'next' );
+		else
+			return FALSE;
+	}
+
+	function refresh()
+	{
+		if( ! empty( $this->api_response->results ))
+			$this->stream( $this->args , 'refresh' );
+		else
+			return FALSE;
+	}
+
+	function stream( $args , $method = 'stream' )
+	{
+		switch( $method )
+		{
+			case 'next':
+			case 'next_page':
+				if( ! empty( $this->api_response->next_page ))
+				{
+					$query_url = 'http://search.twitter.com/search.json' . $this->api_response->next_page;
+					break;
+				}
+			
+			case 'refresh':
+				if( ! empty( $this->api_response->refresh_url ))
+				{
+					$query_url = 'http://search.twitter.com/search.json' . $this->api_response->refresh_url;
+					break;
+				}
+
+			case 'stream':
+			default:
+				$defaults = array(
+					'user_id' => '',
+					'screen_name' => '',
+					'since_id' => '',
+					'max_id' => '',
+					'count' => 10,
+					'page' => '',
+					'trim_user' => TRUE,
+					'contributor_details' => FALSE,
+					'include_entities' => TRUE,
+					'exclude_replies' => FALSE,
+					'include_rts' => TRUE,
+				);
+				$args = wp_parse_args( $args, $defaults );
+
+				// save the args
+				$this->args = $args;
+
+				$query_url = add_query_arg( $args , 'http://api.twitter.com/1/statuses/user_timeline.json' );
+		}
+
+		$temp_results = wp_remote_get( $query_url );
+		if ( is_wp_error( $temp_results ))
+		{
+			$this->error = $temp_results; 
+			return FALSE;
+		}
+
+		$this->api_response = json_decode( wp_remote_retrieve_body( $temp_results ));
+		unset( $temp_results );
+
+		if( ! empty( $this->api_response->error ))
+		{
+			$this->error = $this->api_response; 
+			unset( $this->api_response );
+			return FALSE;
+		}
+/*
+		foreach( $this->api_response->results as $result )
+		{
+			$this->api_response->min_id = $result->id;
+			$this->api_response->min_id_str = $result->id_str;
+		}
+*/
+		return $this->api_response->results;
+	}
 }
 
 /**
